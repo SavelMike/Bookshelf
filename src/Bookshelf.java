18c12f18b2e4b031dc2653d8a7994873eaa76baf
Saveliev Mikhail <Savelievmv@gmail.com>
2016-12-07 17:45:12 +0100

    Initial commit

---
+++ src/Bookshelf.java
@@ -0,0 +1,45 @@
+import java.awt.print.Book;
+import java.util.ArrayList;
+
+/**
+ * Created by Михаил on 07.12.2016.
+ */
+public class Bookshelf {
+
+    public Bookshelf() {
+
+    }
+
+    public void add(Book b) {
+
+    }
+
+    public void remove(int i) {
+
+    }
+
+    public void moveToBookshelf(int i, Bookshelf store) {
+
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public String getoldestAuthor() {
+        return oldestAuthor;
+    }
+
+    public ArrayList getListOfBooks() {
+        return ListOfBooks;
+    }
+
+    public ArrayList getAuthorBooks() {
+        return AuthorBooks;
+    }
+
+}
