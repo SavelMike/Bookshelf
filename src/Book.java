379978d095c986540184c7a1b2ff25635552c8e2
Saveliev Mikhail <Savelievmv@gmail.com>
2016-12-07 19:33:12 +0100

    Class Book is ready

---
+++ src/Book.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Михаил on 07.12.2016.
+ */
+public class Book {
+
+    private String Title;
+    private int pages;
+    private String ISBNnum;
+    private String Author;
+
+    public Book(String t, int p, String isbn) {
+        Title = t;
+        pages = p;
+        ISBNnum = isbn;
+    }
+
+    public String getTitle() {
+
+    }
+
+    public int getPages() {
+
+    }
+
+    public String getISBNnumber() {
+
+    }
+
+    public String getAuthor() {
+
+    }
+}
